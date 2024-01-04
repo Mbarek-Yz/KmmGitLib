a091fe506081a591c16b77bb8bb7aceeac583b13
mobelite <ymbarek@mobelite.fr>
2024-01-04 14:29:08 +0100

    3.0.1

--- shared/src/androidMain/kotlin/com/example/kmmgitlib/Toast.android.kt
+++ shared/src/androidMain/kotlin/com/example/kmmgitlib/Toast.android.kt
@@ -1,6 +1,7 @@
 package com.example.kmmgitlib
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 actual fun getToast(message: String, context: Any?) {
@@ -8,5 +9,6 @@ actual fun getToast(message: String, context: Any?) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     } else {
         // Handle the case where the context is not of the expected type
+        Log.w("ModuleKmm","No Toast")
     }
 }
